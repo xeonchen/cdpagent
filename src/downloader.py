614fea934b2069c02f8c260737cd8b8900575947
Liang-Heng Chen <xeonchen@gmail.com>
2017-02-22 18:40:48 +0800

    download/install/setup on agent

---
+++ src/downloader.py
@@ -0,0 +1,122 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+
+import hashlib
+import json
+import os
+import re
+import shutil
+import stat
+import sys
+import tempfile
+import urllib2
+
+class EnvBoostrapper(object):
+    def __init__(self):
+        super(EnvBoostrapper, self).__init__()
+
+class TryBuildDownloader(object):
+    CONFIG = {
+        'protocol': 'https',
+        'host': 'archive.mozilla.org',
+        'path': '/pub/firefox/try-builds/%(build_id)s/try-%(platform)s',
+    }
+
+    def __init__(self, platform, build_id):
+        super(TryBuildDownloader, self).__init__()
+
+        self.config = self.CONFIG.copy()
+
+        self.config['platform'] = platform
+        self.config['build_id'] = build_id
+        self.config['path'] = self.config['path'] % (self.config)
+        self.config['lang'] = 'en-US'
+        self.config['version'] = self._check_version()
+        self.config['installer'] = 'firefox-%(version)s.%(lang)s.%(platform)s.installer.exe' % (self.config)
+
+        self.checksums = {}
+        self.digests = {}
+        self.path = ''
+        self.filesize = 0
+
+    def _check_version(self):
+        url = '%(protocol)s://%(host)s%(path)s/' % (self.config)
+        req = urllib2.urlopen(url)
+        for line in req.read().split('\n'):
+            pat = 'firefox-(\d+.\w+).%(lang)s.%(platform)s.installer.exe' % self.config
+            m = re.search(pat, line)
+            if m:
+                return m.group(1)
+        return None
+
+    def _prepare_checksums(self):
+        url = '%(protocol)s://%(host)s%(path)s/firefox-%(version)s.%(lang)s.%(platform)s.checksums' % (self.config)
+        req = urllib2.urlopen(url)
+
+        checksums = {}
+        for line in req.read().split('\r\n'):
+            if not line: continue
+
+            checksum, name, size, filename = line.split()
+            checksum = checksum.lower()
+            name = name.lower()
+            size = int(size, 10)
+            filename = filename.split('/')[-1]
+
+            if filename != self.config['installer']: continue
+            checksums[name] = {
+                'checksum': checksum.lower(),
+                'size': int(size),
+            }
+        self.checksums = checksums
+
+    def fetch(self):
+        self._prepare_checksums()
+
+        url = '%(protocol)s://%(host)s%(path)s/%(installer)s' % (self.config)
+        req = urllib2.urlopen(url)
+
+        fd, path = tempfile.mkstemp(prefix='moz')
+        filesize = 0
+        hashes = dict((m, hashlib.new(m)) for m in self.checksums)
+
+        with os.fdopen(fd, 'w', 65536) as f:
+            while True:
+                buf = req.read(65536)
+                if not buf: break;
+
+                filesize += len(buf)
+                for m in hashes.itervalues():
+                    m.update(buf)
+
+                f.write(buf)
+                print 'downloading: %d bytes\r' % filesize,
+        print ''
+
+        for k, v in hashes.iteritems():
+            self.digests[k] = v.hexdigest().lower()
+
+        self.path = path
+        self.filesize = filesize
+        return True
+
+    def check(self):
+        for k, v in self.digests.iteritems():
+            if self.checksums[k]['checksum'] != v:
+                print 'checksum %s mismatch' % k
+                return False
+            if self.checksums[k]['size'] != self.filesize:
+                print 'size %s mismatch' % k
+                return False
+        return True
+
+    def save(self, path):
+        shutil.move(self.path, path)
+        base, ext = os.path.splitext(path)
+        with open(base + '.checksum', 'w') as f:
+            json.dump(self.digests, f, indent=2)
+            f.write('\n')
+
+    def reset(self):
+        if os.path.exists(self.path):
+            os.remove(self.path)
---
+++ src/wpt.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+
+import json
+import os
+import sys
+
+from downloader import TryBuildDownloader
+
+INSTALLER_DIR =   'C:\\CDP\\installer'
+WPT_DRIVER_BASE = 'C:\\CDP\\etc\\wptdriver.ini'
+WPT_DRIVER_INI =  'C:\\webpagetest\\wptdriver.ini'
+PLATFORM = 'win32'
+
+INSTALL_INI = '''[Install]
+InstallDirectoryPath=C:\\CDP\\Try-%(build_id)s\\
+QuickLaunchShortcut=false
+DesktopShortcut=false
+StartMenuShortcuts=false
+MaintenanceService=false
+'''
+
+WPT_INI = '''[Try-%(build_id_no_dot)s]
+exe="C:\\CDP\\Try-%(build_id)s\\firefox.exe"
+options='-profile "%%PROFILE%%" -no-remote'
+template=firefox
+'''
+
+def download(build_id):
+  base = os.path.join(INSTALLER_DIR, 'try-%s' % build_id)
+
+  installer_path = '%s.%s' % (base, 'exe')
+  if not os.path.exists(installer_path):
+    d = TryBuildDownloader(PLATFORM, build_id)
+    if d.fetch() and d.check():
+        d.save(installer_path)
+        print 'success'
+    else:
+        d.reset()
+        print 'failed'
+        return False
+
+    config = {
+      'build_id': build_id,
+      'build_id_no_dot': build_id.replace('.', '_')
+    }
+    with open(base + '.ini', 'w') as f:
+        f.write(INSTALL_INI % config)
+
+    with open(base + '.wpt', 'w') as f:
+        f.write(WPT_INI % config)
+
+  return True
+
+def install(build_id):
+  base = os.path.join(INSTALLER_DIR, 'try-%s' % build_id)
+  return os.system('%s.exe /INI=%s.ini' % (base, base)) == 0
+
+def get_file_list():
+  files = filter(lambda f: f.endswith('.wpt'), os.listdir(INSTALLER_DIR))
+  paths = map(lambda f: os.path.join(INSTALLER_DIR, f), files)
+  return [WPT_DRIVER_BASE] + paths
+
+def read_ini_content():
+  for ini in get_file_list():
+    with open(ini, 'r') as f:
+      yield f.read()
+
+def update():
+  with open(WPT_DRIVER_INI, 'w') as f:
+    for c in read_ini_content():
+      f.write(c + '\r\n')
+
+def main(arg):
+  build_id = arg
+
+  if download(build_id) and install(build_id):
+    update()
+
+if __name__ == '__main__':
+    main(sys.argv[1])
