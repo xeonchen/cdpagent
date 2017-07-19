eb92a9bbda1acc8d7e8aed1db7e6e01f836596b2
Liang-Heng Chen <xeonchen@gmail.com>
2017-07-19 16:13:36 +0800

    background downloader

--- webpagetest/templates/Firefox/extensions/webext@quantum.net/main.js
+++ webpagetest/templates/Firefox/extensions/webext@quantum.net/main.js
@@ -1,3 +1,67 @@
 "use strict";
+/* globals browser */
 
-console.log('Hello World');
+console.log('Hello Download');
+
+(function () {
+  let downloadId = null;
+  let counter = 0;
+
+  function init() {
+    console.log('init: ' + counter);
+    if (counter++) {
+      return;
+    }
+
+    browser.downloads.download({
+      url: 'http://192.168.1.254:8080/large_file',
+      incognito: true,
+      filename: 'delete_me.zip',
+      conflictAction: 'overwrite',
+      saveAs: false
+    }).then(id => {
+      console.assert(!downloadId);
+      downloadId = id;
+      console.log('Downloading: ' + downloadId);
+    }, err => {
+      console.log('Error: ' + err);
+    }).catch(exc => {
+      console.log('Exception: ' + exc);
+      downloadId = null;
+    });
+  }
+
+  function close() {
+    console.log('close: ' + (counter-1));
+    if (--counter) {
+      return;
+    }
+
+    if (downloadId) {
+      browser.downloads.cancel(downloadId).then(() => {
+        console.log('Canceled Download');
+        downloadId = null;
+      }, err => {
+        console.log('Error: ' + err);
+      }).catch(exc => {
+        console.log('Exception: ' + exc);
+      });
+    }
+  }
+
+  browser.webNavigation.onBeforeNavigate.addListener(details => {
+    console.log('onBeforeNavigate: ' + details.url);
+    init();
+  });
+
+  browser.webNavigation.onCompleted.addListener(details => {
+    console.log('onCompleted: ' + details.url);
+    close();
+  });
+
+  browser.webNavigation.onErrorOccurred.addListener(details => {
+    console.log('onErrorOccurred: ' + details.url);
+    close();
+  });
+
+})();
